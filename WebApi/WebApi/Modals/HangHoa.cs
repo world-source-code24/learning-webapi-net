6e78ffd1b502611d495afd374a7101ee334a44a7
Sang <huusang2k4@gmail.com>
2024-09-14 17:52:02 +0700

    Khoi tao API dau tien

---
+++ WebApi/WebApi/Modals/HangHoa.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Modals
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+
+    }
+}
